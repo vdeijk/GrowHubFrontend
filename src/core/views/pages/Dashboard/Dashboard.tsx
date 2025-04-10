fc0b6af848296d798b6357b4b7a02647dc76ca6e
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-10 16:40:36 +0200

    added swagger codegen

--- src/core/views/pages/Dashboard/Dashboard.tsx
+++ src/core/views/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import turnoverStore from '../../../stores/TurnoverStore/TurnoverStore';
 import FarmLocations from '../FarmLocations/FarmLocations';
 import CurrentWeather from '../../containers/CurrentWeather/CurrentWeather';
 import Tasks from '../../containers/Tasks/Tasks';
 import Plants from '../../containers/Plants/Plants';
-import TurnoverGraph from '../../containers/TurnoverGraph/TurnoverGraph';
 
 const Dashboard: React.FC = () => {
   return (
@@ -12,10 +10,15 @@ const Dashboard: React.FC = () => {
       <CurrentWeather />
       <FarmLocations/>
       <Tasks />
-      <TurnoverGraph data={turnoverStore.turnovers} width={500} height={300} />
       <Plants />
     </>
   );
 };
 
 export default Dashboard;
+
+/*
+import TurnoverGraph from '../../containers/TurnoverGraph/TurnoverGraph';
+import turnoverStore from '../../../stores/TurnoverStore/TurnoverStore';
+      {/* <TurnoverGraph data={turnoverStore.turnovers} width={500} height={300} />
+      <ButtonContainer {...buttonContainerData} />*/
