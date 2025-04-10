8e22d2b7b349b554daa487ee16c4c23cbf0c04c2
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-10 14:19:41 +0200

    small improvements for weather data

--- src/core/stores/WeatherStore/WeatherStore.ts
+++ src/core/stores/WeatherStore/WeatherStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable } from 'mobx';
 import { runInAction } from 'mobx';
 import { WeatherData } from '../../../auxiliary/interfaces/WeatherData';
-import { getWeatherData } from '../../apis/weather';
 import { getData } from '../../apis/getData';
 
 class WeatherStore {
@@ -15,37 +14,21 @@ class WeatherStore {
     this.fetchData();
   }
 
-
   private async fetchData() {
     try {
       runInAction(() => {
         this.isLoading = true;
       });
-      const weather = await getData('/Weather/current', { city: "The Hague" });
+      
+      const weather = await getData('/Weather/current', { city: 'The Hague' });
 
       runInAction(() => {
         this.weather = weather;
 
-        console.log(this.weather);
         this.isLoading = false;
       });
     } catch (error) {
       console.error('Error fetching data:', error);
-    }
-  }
-
-  async fetchWeather() {
-    try {
-      runInAction(() => {
-        this.isLoading = true;
-      });
-      const weather = await getWeatherData(this.city);
-      runInAction(() => {
-        this.weather = weather;
-        this.isLoading = false;
-      });
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
     }
   }
 }
