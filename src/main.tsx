f439caf7733778777f4024b2a63c56cea928a35a
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-12 12:49:35 +0200

    Set up GitHub Actions pipeline for FE deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import './variables.css'; 
+import './variables.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import settingsStore from './core/stores/SettingsStore/SettingsStore';
