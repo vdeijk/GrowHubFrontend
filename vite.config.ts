2d20be879217059175dd436297cf6e6b7788484b
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-15 20:20:17 +0200

    project name fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/farmManagement/' : '/',
+  base: mode === 'production' ? '/GrowHubFrontend/' : '/',
   build: {
     outDir: 'dist',
   },
