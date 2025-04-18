49112e640bba5513615e220def6c602c47f06b02
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-18 20:49:43 +0200

    serving from root

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: mode === 'production' ? '/GrowHubFrontend/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
