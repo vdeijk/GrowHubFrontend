afadc086051c3ea2f11c6b772084806ec51db1cd
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-05-06 21:28:49 +0200

    footer improved

--- src/core/views/containers/FooterContainer/FooterContainer.tsx
+++ src/core/views/containers/FooterContainer/FooterContainer.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import styles from './FooterContainer.module.css';
+import Heading from '../../reusables/Heading/Heading';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import SocialIcon from '../../reusables/SocialIcon/SocialIcon';
+import FooterLink from '../../reusables/FooterLink/FooterLink';
 
 const FooterContainer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <p>
-        &copy; {new Date().getFullYear()} VanEyk Web Development. All rights
-        reserved.
-      </p>
+      <div className={styles.footerCol}>
+        <Heading
+          level={6}
+          text="Follow"
+          customStyles={{ color: 'var(--color-white)', marginBottom: '1rem' }}
+        />
+        <ul className={styles.socialIcons}>
+          <SocialIcon
+            link="https://github.com/vdeijk"
+            icon={FaGithub}
+            label="GitHub"
+          />
+          <SocialIcon
+            link="https://www.linkedin.com/in/rick-van-der-eijk-9829b774/"
+            icon={FaLinkedin}
+            label="LinkedIn"
+          />
+        </ul>
+      </div>
+
+      <div className={styles.footerCol}>
+        <Heading
+          level={6}
+          text="Contact"
+          customStyles={{ color: 'var(--color-white)', marginBottom: '1rem' }}
+        />
+        <p className={styles.contactText}>Name: Rick van der Eijk </p>
+        <p className={styles.contactText}>Phone: +31)628258357 </p>
+        <p className={styles.contactText}>Email: rickvdeijk@gmail.com </p>
+        <br></br>
+        <p className={styles.contactText}>
+          Available for freelance work, customizations, or full-time roles.{' '}
+        </p>
+      </div>
+
+      <div className={styles.resources}>
+        <Heading
+          level={6}
+          text="Resources"
+          customStyles={{ color: 'var(--color-white)', marginBottom: '1rem' }}
+        />
+        <ul className={styles.resources}>
+          <FooterLink
+            link="https://vdeijk.github.io/growhubDocs/"
+            label="Documentation"
+          />
+        </ul>
+      </div>
     </footer>
   );
 };
